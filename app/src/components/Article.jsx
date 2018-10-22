dc0f12f528bb7a10e108f77f9647b2ab85b9656a
Deimos <alex279368@gmail.com>
2018-10-22 22:49:16 +0300

    Learn...

---
+++ app/src/components/Article.jsx
@@ -0,0 +1,48 @@
+import React, {Component} from 'react'
+
+class Article extends Component {
+
+    state = {
+        isOpen: true
+    }
+
+    // constructor(props) {
+    //     super(props) 
+
+    //     this.state = {
+    //         isOpen: true
+    //     }
+
+    //     this.handleClick = handleClick.bind(this)
+    // }
+    // Аналогічно конструктору
+    // state = {
+    //     isOpen: true
+    // }
+
+    render() {
+        const {article} = this.props
+        const body = this.state.isOpen &&<section>{article.text}</section>
+        return (
+            <div>
+                <h2>
+                {article.title}
+                    <button onClick={this.handleClick}>
+                        {this.state.isOpen ? 'close' : 'open'}
+                    </button>
+                </h2>
+                {body}
+                <h3>creation date: {(new Date(article.date)).toLocaleDateString()} </h3>
+            </div>
+        )
+    }
+    handleClick = () => {
+        console.log('---', 'clicked')
+        this.setState({
+            isOpen: !this.state.isOpen
+        })
+    }
+}
+
+
+export default Article
